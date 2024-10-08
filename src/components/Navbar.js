086bea9634dbb31951b24145ef7828810ef1d5f6
Aman <singhalaman198@gmail.com>
2024-10-08 20:15:03 +0530

    adding the pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from "../assets/Logo.svg"
 
-const Navbar = () => {
+const Navbar = ({isUserLoggedIn, setUserLoggedIn}) => {
+
   return (
-    <div>
-      
+    <div className='flex justify-evenly'>
+      <Link to="/">
+        <img src={logo} alt='logo' width={160} height={32} loading='lazy'/>
+      </Link>
+      <nav>
+        <ul className='flex gap-3'>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/">About</Link></li>
+          <li><Link to="/">Contact</Link></li>
+        </ul>
+      </nav>
+
+      <div className='flex ml-5 mr-3 gap-3'>
+        {
+          !isUserLoggedIn &&
+          <Link to="/login"><button>Login</button></Link>
+        }
+        {
+          !isUserLoggedIn &&
+          <Link to="/signup"><button>Signup</button></Link>
+        }
+        {
+          isUserLoggedIn &&
+          <Link to="/"><button>Logout</button></Link>
+        }
+        {
+          isUserLoggedIn &&
+          <Link to="/dashboard"><button>Dashboard</button></Link>
+        }
+      </div>
     </div>
   )
 }
