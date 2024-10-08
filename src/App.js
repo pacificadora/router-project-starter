1fbca9507730202d3faaf7b2548380f65d21b51a
Aman <singhalaman198@gmail.com>
2024-10-08 19:21:33 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      
+    </BrowserRouter>
+  );
+}
+
+export default App;
